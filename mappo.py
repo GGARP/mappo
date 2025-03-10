3433d152e5ceba3d8cd43db97d46b8e7bbd6c757
GGARP <akovbian.georgii@gmail.com>
2025-03-10 12:38:45 +0100

    Update mappo.py

--- mappo.py
+++ mappo.py
@@ -55,7 +55,7 @@ def tune_ppo(config):
     """
     Tune the PPO trainer
     """
-    env_config_name = "cyclical_demand"
+    env_config_name = "demand_shock"
     env_config = env_configs[env_config_name]
     num_episodes = 100
 
@@ -125,7 +125,7 @@ if __name__ == '__main__':
         },
         run_config=ray.air. RunConfig(
     storage_path=os.path.join(os.getcwd(), "..", "results"),
-    name="MAPPO_cyc3",
+    name="MAPPO_shock",
 ),
     )
 
